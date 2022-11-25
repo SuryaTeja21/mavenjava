8967cfd90113ea451125318b7e20a261ea6a90e9
suryateja <csuryateja2002@gmail.com>
2022-11-25 13:17:08 +0530

    mavenjava

---
+++ src/main/java/com/maven/maven5/App.java
@@ -0,0 +1,13 @@
+package com.maven.maven5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab internal 1 exam" );
+    }
+}
